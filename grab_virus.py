4f51708ad36aed68b8a1859457dfabadcb80c45d
cullen <newxieyang@msn.cn>
2020-04-16 21:39:54 +0800

    优化疫情数据

--- grab_virus.py
+++ grab_virus.py
@@ -4,69 +4,97 @@ from net_utils import *
 import prettytable as pt
 import json
 
-qq_url = 'https://api.inews.qq.com/newsqa/v1/query/pubished/daily/list?country='
-select_country = ['美国', '意大利', "德国",'法国','英国','日本本土', '俄罗斯']
-split_country = ['奥地利','匈牙利','捷克','斯洛伐克','塞尔维亚','黑山','克罗地亚','斯洛文尼亚','北马其顿','波黑']
-
-# # 读取网络资源
-# def fetch_source(url):
-#     headers = {
-#         'User-Agent': generate_user_agent(device_type='desktop', os=('mac', 'linux', 'win', 'android'))
-#     }
-#
-#     s = session()
-#     resp = s.get(url, headers=headers)
-#     resp.encoding = resp.apparent_encoding
-#     if resp.status_code == 200:
-#        return parse_data(resp.text)
-#     else:
-#         fetch_source(url)
-
-
-def parse_data(data):
-    data_dic = json.loads(data)
-    items = data_dic['data']
-    if items is not None:
-        return items[-1]
+# 奥匈解体后分裂成10个小国， 要分开计算
+url = 'https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist'
+major_members = ['美国', '意大利', "德国",'法国','英国','日本本土', '俄罗斯']
+o_members = ['奥地利','匈牙利','捷克','斯洛伐克','塞尔维亚','黑山','克罗地亚','斯洛文尼亚','北马其顿','波黑']
+population = {'美国':33000, '意大利':6043, "德国":8292,'法国':6698,'英国':6648,'日本本土':12477, '俄罗斯':14447, '奥匈':5440}
 
 # 获取列表的第二个元素
 def take_second(elem):
-    return elem[1]
+    return elem[2]
 
 
-if __name__ == '__main__':
+def top_ten_data(items):
+
+    tb2 = pt.PrettyTable()
+    tb2.field_names = ["国家", "确诊", "死亡", "死亡率"]
 
     sum_dead = 0
     sum_confirm = 0
-    sum_add = 0
+
+    for item in items[:10]:
+        dead_rate = '{:.2%}'.format(item['dead']/item['confirm'])
+        tb2.add_row([item['name'], item['confirm'], item['dead'], dead_rate])
+        sum_dead +=  item['dead']
+        sum_confirm += item['confirm']
+
+    dead_rate = '{:.2%}'.format(sum_dead/sum_confirm)
+    tb2.add_row(['合计', sum_confirm,  sum_dead, dead_rate])
+    print(tb2)
+
+def major_members_data(items):
+
+    members = major_members + o_members
+
+    index = 0
     tb = pt.PrettyTable()
-    tb.field_names = ["国家", "确诊","新增", "死亡", "死亡率"]
+    tb.field_names = ["国家", "确诊", "死亡", "死亡率"]
+    sum_dead  = 0
+    sum_confirm = 0
+
+    for item in items:
+        if index < 17:
+            if item['name'] in members:
+                sum_dead +=  item['dead']
+                sum_confirm += item['confirm']
+                dead_rate = '{:.2%}'.format(item['dead']/item['confirm'])
+                tb.add_row([item['name'], item['confirm'], item['dead'], dead_rate])
+                index += 1
+        if index == 17:
+            break
+
+    dead_rate = '{:.2%}'.format(sum_dead/sum_confirm)
+    tb.add_row(['合计', sum_confirm, sum_dead, dead_rate])
+    print(tb)
+
+def allies(items):
+    sum_dead = 0
+    sum_confirm = 0
+
+    # 奥匈帝国
+    o_sum_dead = 0
+    o_sum_confirm = 0
+
+    tb = pt.PrettyTable()
+    tb.field_names = ["国家", '人口', "确诊", '确诊率',"死亡", "死亡率"]
 
     list =[]
 
-    # 七国
-    for item in select_country:
-        res = fetch_source(qq_url + item)
-        result = parse_data(res)
-        sum_confirm += result['confirm']
-        sum_dead += result['dead']
-        sum_add += result['confirm_add']
-        percent = '{:.2%}'.format((result['dead']/result['confirm']))
-        list.append([item, result['confirm'], result['confirm_add'], result['dead'], percent])
+    count1 = 0 #联军总数（剔除奥匈）
+    count2 = 0  #奥匈帝国解体后的国家总数
+    # 总共有170多个国家， 把联军分开，累计，联军国家遍历后就退出循环
+    for item in  items:
+        if count1 < 7:
+            if item['name'] in major_members:
+                count1 +=1
+                sum_confirm += item['confirm']
+                sum_dead += item['dead']
+                confirm_rate = '{:.4%}'.format(item['confirm']/(population[item['name']]*10000))
+                dead_rate = '{:.2%}'.format(item['dead']/item['confirm'])
+                list.append([item['name'], population[item['name']], item['confirm'], confirm_rate, item['dead'], dead_rate])
+        if count2 < 10:
+            if item['name'] in o_members:
+                count2 +=  1
+                o_sum_confirm += item['confirm']
+                o_sum_dead += item['dead']
+        if count1 == 7 and count2 == 10:
+            break
 
-    # 奥匈帝国
-    count_split_dead = 0
-    count_split_confirm = 0
-    count_split_add = 0
-    for item in split_country:
-        res = fetch_source(qq_url + item)
-        result = parse_data(res)
-        count_split_confirm += result['confirm']
-        count_split_dead += result['dead']
-        count_split_add += result['confirm_add']
 
-    percent = '{:.2%}'.format(count_split_dead/count_split_confirm)
-    list.append(["奥匈", count_split_confirm, count_split_add, count_split_dead, percent])
+    dead_rate = '{:.2%}'.format(o_sum_dead/o_sum_confirm)
+    confirm_rate = '{:.4%}'.format(o_sum_confirm/(population['奥匈']*10000))
+    list.append(["奥匈", population['奥匈'], o_sum_confirm, confirm_rate, o_sum_dead, dead_rate])
 
     list.sort(key=take_second, reverse=True)
 
@@ -74,11 +102,22 @@ if __name__ == '__main__':
         tb.add_row(item)
 
 
-    sum_confirm += count_split_confirm
-    sum_dead += count_split_dead
-    sum_add +=  count_split_add
+    sum_confirm += o_sum_confirm
+    sum_dead += o_sum_dead
 
-    total_percent = '{:.2%}'.format(sum_dead/sum_confirm)
-    tb.add_row(["合计", sum_confirm, sum_add, sum_dead, total_percent])
+    total_dead_rate = '{:.2%}'.format(sum_dead/sum_confirm)
+    total_confirm_rate = '{:.2%}'.format(sum_confirm/930450000)
+    tb.add_row(["合计",93045, sum_confirm, total_confirm_rate, sum_dead, total_dead_rate])
+    print("人口单位是万， 死亡率是基于确诊人数")
     print(tb)
+
+if __name__ == '__main__':
+
+    res = fetch_source(url)
+    data_dic = json.loads(res)
+    items = data_dic['data']
+    if items is not None:
+        major_members_data(items)
+        top_ten_data(items)
+        allies(items)
 
